77834b1f55df953c67cefa67a8b70e31157d917c
it-devros <it-devros@outlook.com>
2019-06-25 07:51:52 -0400

    code base updated

--- jsLib/soapCity.js
+++ jsLib/soapCity.js
@@ -1,48 +1,48 @@
 
 
 // import the soap and xml2js modules
-var soap = require('soap');
-var parseString = require('xml2js').parseString;
+var soap = require('soap')
+var parseString = require('xml2js').parseString
 
 // example WSDL url
-var url = 'http://www.webservicex.com/globalweather.asmx?wsdl';
+var url = 'http://www.webservicex.com/globalweather.asmx?wsdl'
 
 
 // example function to get the Cities for Each Country
 function getCitiesFromWSDL(res, country) {
 
-	// new promise object
-	var p = new Promise(function(resolve, reject) {
+  // new promise object
+  var p = new Promise(function(resolve, reject) {
 
-		soap.createClient(url, function(err, client) {
-			if(err) throw new Error(err);
+    soap.createClient(url, function(err, client) {
+      if(err) throw new Error(err)
 
-			console.log("Country: ", country);
+      console.log("Country: ", country)
 
-            var args = {
-            	CountryName: country
-            };
+      var args = {
+        CountryName: country
+      }
 
 
-            client.GetCitiesByCountry(args, function(errr, result, body){
-            	if (errr) reject(errr);
-            	if (!result) {
-            		throw new Error("There are no results");
-            	}
+      client.GetCitiesByCountry(args, function(errr, result, body){
+        if (errr) reject(errr)
+        if (!result) {
+          throw new Error("There are no results")
+        }
 
-            	var cities = result.GetCitiesByCountryResult;
-            	
-            	parseString(cities, function(erro, respo){
-            		res.send(respo);
-            	});
-            });
+        var cities = result.GetCitiesByCountryResult
+        
+        parseString(cities, function(erro, respo){
+          res.send(respo)
+        })
+      })
 
-		});
+    })
 
-	});
+  })
 
-	return p;
+  return p
 }
 
 
-module.exports = getCitiesFromWSDL;
+module.exports = getCitiesFromWSDL
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-var getCitiesFromWSDL = require('../jsLib/soapCity');
+var getCitiesFromWSDL = require('../jsLib/soapCity')
 
 // router for inputing country
 router.get('/soap/:country', function(req, res, next) {
 
-	var country = req.params.country;
+  var country = req.params.country
 
-	getCitiesFromWSDL(res, country)
-	.then(function(result){
+  getCitiesFromWSDL(res, country)
+  .then(function(result){
 
-	})
-});
+  })
+})
 
 
 
-module.exports = router;
+module.exports = router
